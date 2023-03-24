9aa156cf0c4f144ba44009cebe299867384ff8b6
J. Zebedee <zebedee@code.gripe>
2023-03-24 16:31:37 -0500

    Add project files.

---
+++ SubModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TaleWorlds.MountAndBlade;
+
+namespace BattleInventory;
+
+public class SubModule : MBSubModuleBase
+{
+    private readonly Lazy<BattleInventoryMissionLogic> _logic = new();
+
+    public override void OnMissionBehaviorInitialize(Mission mission)
+        => mission?.AddMissionBehavior(_logic.Value);
+}
+
+public class BattleInventoryMissionLogic : MissionLogic
+{
+
+}
